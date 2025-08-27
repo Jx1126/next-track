e4fd6ddb9ffd4ddbcd0759a8a3a87d614e01b407
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2025-08-27 18:15:44 +0800

    feat: update router configuration to include Home and Documentation routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '../pages/HomePage.vue';
 import SearchPage from '../pages/SearchPage.vue';
 import SearchResultsPage from '../pages/SearchResultsPage.vue';
 import PlaylistsPage from '../pages/PlaylistsPage.vue';
 import PlaylistDetailsPage from '../pages/PlaylistDetailsPage.vue';
 import RecommendationPage from '../pages/RecommendationPage.vue';
 import RecommendationResultPage from '../pages/RecommendationResultPage.vue';
+import DocumentationPage from '../pages/DocumentationPage.vue';
 
 const routes = [
   {
     path: '/',
-    redirect: '/playlists'
+    name: 'Home',
+    component: HomePage,
   },
   {
     path: '/search',
@@ -40,6 +43,11 @@ const routes = [
     path: '/recommend/:id/result',
     name: 'RecommendationResult',
     component: RecommendationResultPage,
+  },
+  {
+    path: '/docs',
+    name: 'Documentation',
+    component: DocumentationPage,
   },
 ]
 
