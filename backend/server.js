b325921e058f11b12d4e97ade3342ebf9b699d4b
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2025-09-21 15:44:03 +0800

    refactor: refactor `server.js` with Prettier

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,13 +8,13 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const tracks_route = require('./routes/tracks');
-const playlists_route = require('./routes/playlists');
-const recommend_route = require('./routes/recommend');
+const tracks_route = require("./routes/tracks");
+const playlists_route = require("./routes/playlists");
+const recommend_route = require("./routes/recommend");
 
-app.use('/api/music', tracks_route);
-app.use('/api/music/playlist', playlists_route);
-app.use('/api/music/recommend', recommend_route);
+app.use("/api/music", tracks_route);
+app.use("/api/music/playlist", playlists_route);
+app.use("/api/music/recommend", recommend_route);
 
 app.listen(PORT, () => {
   console.log(`API is running on http://localhost:${PORT}`);
